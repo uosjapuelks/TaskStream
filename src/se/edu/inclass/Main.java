41129b2a54b43509a81f8fb12ff2e366ccb6df5a
Anderson <andersonleong@outlook.com>
2021-09-17 17:17:46 +0800

    inClass

--- src/se/edu/inclass/Main.java
+++ src/se/edu/inclass/Main.java
@@ -20,6 +20,8 @@ public class Main {
         printDeadlines(tasksData);
 
         System.out.println("Total number of deadlines: " + countDeadlines(tasksData));
+        printDataWithStreams(tasksData);
+        printDeadlinesUsingStream(tasksData);
 
     }
 
@@ -33,13 +35,35 @@ public class Main {
         return count;
     }
 
+    private static int countDeadlineesUsingStream(ArrayList<Task> tasks) {
+        int count = 0;
+        tasks.stream()
+                .filter((t) -> t instanceof Deadline)
+                .count();
+        return count
+    }
+
     public static void printData(ArrayList<Task> tasksData) {
         for (Task t : tasksData) {
             System.out.println(t);
         }
     }
 
+    private static void printDataWithStreams (ArrayList<Task> tasks) {
+        System.out.println("Printing data using stream");
+        tasks.stream()  //convert data to stream
+                .forEach((System.out::println)); //terminal operator
+    }
+
+    public static void printDeadlinesUsingStream (ArrayList<Task> tasks) {
+        System.out.println("Printing deadlines using stream");
+        tasks.stream()
+                .filter((t)-> t instanceof Deadline) //Filers using LAMBDA
+                .forEach(System.out::println);
+    }
+
     public static void printDeadlines(ArrayList<Task> tasksData) {
+        ArrayList<Task> deadlinees = new ArrayList<>();
         for (Task t : tasksData) {
             if (t instanceof Deadline) {
                 System.out.println(t);
